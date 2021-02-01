230fa510f5f54a0f2ee01a3a603a67fa34afd21b
goreleaserbot <mail@aota.blog>
2021-02-02 00:28:09 +0900

    Brew formula update for tuna version v0.0.7-alpha

--- tuna.rb
+++ tuna.rb
@@ -5,21 +5,21 @@
 class Tuna < Formula
   desc "Software to generate and serve JSON/HTML with flow model"
   homepage "https://github.com/solaoi/tuna-mayonnaise"
-  version "0.0.6-alpha"
+  version "0.0.7-alpha"
   license "MIT"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.6-alpha/tuna_0.0.6-alpha_macOS-64bit.tar.gz"
-    sha256 "25c18f4e957f54905c375d8620225bbb8bad503cfdde33b868ba5d1f414ea987"
+    url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.7-alpha/tuna_0.0.7-alpha_macOS-64bit.tar.gz"
+    sha256 "51a3dc0c67606a5da2a4b5700b7fe6e15b5cc1b7d289307d957f474206516349"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.6-alpha/tuna_0.0.6-alpha_Linux-64bit.tar.gz"
-    sha256 "0b3af98512929429ad6e156fae245603f79ba0e18133e6d075c0dab23985c173"
+    url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.7-alpha/tuna_0.0.7-alpha_Linux-64bit.tar.gz"
+    sha256 "292fa888a7a5e098bc244511b26e42c63e5dec98c5757c02431de03786e439ce"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.6-alpha/tuna_0.0.6-alpha_Linux-ARM64.tar.gz"
-    sha256 "7ca20b0149a22fcc3d84434c9d826eb715ce5133e91400de7e61551d11222d56"
+    url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.7-alpha/tuna_0.0.7-alpha_Linux-ARM64.tar.gz"
+    sha256 "25451f2c66bcaff0e83a9ae5b82045bc653f08c2fc0cc413aef9ec9ca9399506"
   end
 
   def install
