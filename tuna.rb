806cd1b0bc220cbbc4a93bc275a9144599f94e67
goreleaserbot <mail@aota.blog>
2021-07-05 00:15:34 +0900

    Brew formula update for tuna version v0.1.0-alpha

--- tuna.rb
+++ tuna.rb
@@ -5,29 +5,29 @@
 class Tuna < Formula
   desc "Software to generate and serve JSON/HTML with flow model"
   homepage "https://github.com/solaoi/tuna-mayonnaise"
-  version "0.0.20-alpha"
+  version "0.1.0-alpha"
   license "MIT"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.20-alpha/tuna_0.0.20-alpha_macOS-64bit.tar.gz"
-      sha256 "b8e80fd9ec4a5ff6db569602cea121dc1d193cd0ad8a727773026fd1ac47d84b"
+      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.1.0-alpha/tuna_0.1.0-alpha_macOS-64bit.tar.gz"
+      sha256 "5e2a9da3a25aaec236e232ace832f7d22275be09c426448287bca25f6165e430"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.20-alpha/tuna_0.0.20-alpha_macOS-ARM64.tar.gz"
-      sha256 "4faa3cce34998a99ba2cf47fff0fb39990f81045d539787249a7c280ac1d36c2"
+      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.1.0-alpha/tuna_0.1.0-alpha_macOS-ARM64.tar.gz"
+      sha256 "4353bbcfafad14a20b87184c42e5a5e6be7b42ec106a33d21d92950394684c63"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.20-alpha/tuna_0.0.20-alpha_Linux-64bit.tar.gz"
-      sha256 "6cae0d3736fce416115b6e2b19b602e5c58aa4245ddfde6ee10daf72761e5652"
+      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.1.0-alpha/tuna_0.1.0-alpha_Linux-64bit.tar.gz"
+      sha256 "3400960616dee08e8a0dfced4559f1e8002d997275a431c0bda89ed714befda5"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.20-alpha/tuna_0.0.20-alpha_Linux-ARM64.tar.gz"
-      sha256 "c82816a6858d9f06f01dbfab7c106e204d954e04e687033e497cf7b34cdc0394"
+      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.1.0-alpha/tuna_0.1.0-alpha_Linux-ARM64.tar.gz"
+      sha256 "9eccdad9dd6a17d7ffb116535c16be80ce60b54cb117e2feee1acb3e5f85e01c"
     end
   end
 
