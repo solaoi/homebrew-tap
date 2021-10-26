d5cc35e36297045f7326efb0fe0268b34d5c0e38
goreleaserbot <mail@aota.blog>
2021-10-26 10:02:35 +0900

    Brew formula update for tuna version v0.0.4

--- tuna.rb
+++ tuna.rb
@@ -5,26 +5,29 @@
 class Tuna < Formula
   desc "Software to generate and serve JSON/HTML on the node-based editor"
   homepage "https://github.com/solaoi/tuna-mayonnaise"
-  version "0.0.3"
+  version "0.0.4"
   license "MIT"
-  bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.3/tuna_darwin_amd64.tar.gz"
-      sha256 "6ea836759bd37f206e572fb57fa5f586e24ede2d622356738d7d2282c9cd4091"
+      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.4/tuna_darwin_amd64.tar.gz"
+      sha256 "09f60cdc6a98380758e734affc518d78639e6bb497778b9e7a095342658fc4e6"
+
+      def install
+        bin.install "tuna"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.3/tuna_linux_amd64.tar.gz"
-      sha256 "f16867fd86c607f7572fcd4a6103fbd2ee0aeff8b8da5940cd0088939696bc0f"
-    end
-  end
+      url "https://github.com/solaoi/tuna-mayonnaise/releases/download/v0.0.4/tuna_linux_amd64.tar.gz"
+      sha256 "699018fb3c4a10d414dcd051af0231e5458ba02e23702b6d9da38ad1985d3862"
 
-  def install
-    bin.install "tuna"
+      def install
+        bin.install "tuna"
+      end
+    end
   end
 
   test do
