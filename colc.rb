e9bb56b504532a22b81e1924727ead6aaf040f4a
solaoi <46414076+solaoi@users.noreply.github.com>
2022-03-25 17:40:38 +0900

    Brew formula update for colc version

--- colc.rb
+++ colc.rb
@@ -1,8 +1,8 @@
 class Colc < Formula
-  version '1.0.1'
+  version '1.0.2'
   homepage 'https://github.com/solaoi/colc'
-  url "https://github.com/solaoi/colc/releases/download/v1.0.1/colc_darwin_amd64.tar.gz"
-  sha256 '67f851b571b968a122667b878fd182a1b15e96430c7df3ca22aaf952025d99e8'
+  url "https://github.com/solaoi/colc/releases/download/v1.0.2/colc_darwin_amd64.tar.gz"
+  sha256 '1b32dfc0096a502a160aab3a0705340d46b156743bce35603ba1c58ecdab0991'
   head 'https://github.com/solaoi/colc.git'
 
   head do
