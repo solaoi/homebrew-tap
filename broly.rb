3aa884c885b108a49b68f0914ce990a590fd2e18
solaoi <46414076+solaoi@users.noreply.github.com>
2022-12-13 10:01:33 +0900

    Brew formula update for broly version

--- broly.rb
+++ broly.rb
@@ -1,8 +1,8 @@
 class Broly < Formula
-  version '0.2.1'
+  version '0.2.2'
   homepage 'https://github.com/solaoi/broly'
-  url "https://github.com/solaoi/broly/releases/download/v0.2.1/broly_darwin_amd64.tar.gz"
-  sha256 '8749aad2fbda8aeb7a11f7d951dc529aaccbbf6709e019e0325a33c0d91be0b7'
+  url "https://github.com/solaoi/broly/releases/download/v0.2.2/broly_darwin_amd64.tar.gz"
+  sha256 'a70dd64692b0a1debeda9eacf09198690ea69348b84179d7ec6000b85a37ec7f'
   head 'https://github.com/solaoi/broly.git'
 
   head do
