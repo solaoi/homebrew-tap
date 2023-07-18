179c6f7e19981bbdbd30a558b98ede6af5b04ad7
solaoi <46414076+solaoi@users.noreply.github.com>
2023-07-18 09:38:25 +0900

    Brew formula update for voicepeaky version

--- voicepeaky.rb
+++ voicepeaky.rb
@@ -1,8 +1,8 @@
 class Voicepeaky < Formula
-  version '1.0.4'
+  version '1.0.5'
   homepage 'https://github.com/solaoi/voicepeaky'
-  url "https://github.com/solaoi/voicepeaky/releases/download/v1.0.4/voicepeaky_darwin_amd64.tar.gz"
-  sha256 '37aefaea44bd73053589af276fe4e339df758695ecaa5ecacf4d0adcf3869d0d'
+  url "https://github.com/solaoi/voicepeaky/releases/download/v1.0.5/voicepeaky_darwin_amd64.tar.gz"
+  sha256 'd39c2998e67e0732c5dbd6e1ffe31be44951db8de4d85400fc300149dcb0441f'
   head 'https://github.com/solaoi/voicepeaky.git'
 
   head do
